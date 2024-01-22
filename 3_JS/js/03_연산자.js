9e77249c491e373a92ab30f90226ce5d0f66d0af
saem <saem.hong.95@gmail.com>
2024-01-22 16:25:43 +0900

    240122-final

--- 3_JS/js/03_연산자.js
+++ 3_JS/js/03_연산자.js
@@ -19,22 +19,65 @@ function plusFn() {
     result.innerText = Number(value1)+Number(value2);
 };
 function minusFn() {
-    const value1 = number1.value
-    const value2 = number2.value
-    result.innerText = Number(value1)-Number(value2);
+    const value1 = Number(number1.value)
+    const value2 = Number(number2.value)
+    result.innerText = value1-value2;
 }
 function multiFn() {
-    const value1 = number1.value
-    const value2 = number2.value
-    result.innerText = Number(value1)*Number(value2);
+    const value1 = Number(number1.value)
+    const value2 = Number(number2.value)
+    result.innerText = value1*value2;
 }
 function divFn() {
-    const value1 = number1.value
-    const value2 = number2.value
-    result.innerText = Number(value1)/Number(value2);
+    const value1 = Number(number1.value)
+    const value2 = Number(number2.value)
+    result.innerText = value1/value2;
 }
 function modFn() {
-    const value1 = number1.value
-    const value2 = number2.value
-    result.innerText = Number(value1)%Number(value2);
+    const value1 = Number(number1.value)
+    const value2 = Number(number2.value)
+    result.innerText = value1%value2;
+}
+//
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const num3 = document.getElementById("num3");
+const total = document.getElementById("total")
+
+function totalFn() {
+    const value1 = Number(num1.value);
+    const value2 = Number(num2.value);
+    const value3 = Number(num3.value);
+    total.innerText = value1+value2+value3;
+}
+// 증그/감소 연산자 (++,--)
+// 증가, 감소할 수를 저장할 변수 선언
+let count = 0;
+const result2 = document.getElementById("result2")
+function increase() {
+    count++;
+    result2.innerText = count;
+}
+function decrease() {
+    count--;
+    result2.innerText = count;
+}
+// 전위, 후위 연산 확인하기
+function check() {
+    let count = 100;
+    // CPU에게 연산이란 : 코드를 하나하나 실행하는 행위
+    // 전위 연산(++count, --count)
+    // console.log("++count : ", ++count);
+    // 후위 연산(count++, count--)
+    count = 50;
+    console.log("count++ : ", count++);
+
+    console.log("-------------")
+    let a = 10;
+    let b = 5;
+    let c = ++a * b--;
+    // 최종적으로 a,b,c,에 저장된 값은 얼마?
+    // a == 11
+    // b == 4
+    // c == 55
 }
