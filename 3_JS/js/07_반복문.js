0c782feaa45013dcb33f7037bd90a892a40f6787
saem <saem.hong.95@gmail.com>
2024-01-24 10:40:49 +0900

    반복문

--- 3_JS/js/07_반복문.js
+++ 3_JS/js/07_반복문.js
@@ -1,7 +1,7 @@
-for(let num=1;num<=5; num++) {
-    // 조건식이 true; 일 경우만 반복 수행할 코드;
-    console.log("통과!");
-}
+// for(let num=1;num<=5; num++) {
+//     // 조건식이 true; 일 경우만 반복 수행할 코드;
+//     console.log("통과!");
+// }
 function check1() {
     let result = "";
     for(let num=1;num<=5;num++) {
@@ -76,5 +76,100 @@ function executeFn2() {
     // start 부터 end까지 vla씩 증가
     for(let num=start; num<=end; num+=val) {
         ul.innerHTML+=`<li>${num}</li>`;
+    }
+}
+function executeFn3() {
+    const input = Number(document.getElementById("input3").value);
+    const ul = document.getElementById("result3");
+
+    ul.innerHTML = '';
+    if (input < 2 || input > 9 ) {
+        alert ("")
+        return;
+    }
+    for (let num = 1; num <=9; num++ ) {
+        ul.innerHTML += `<li>${input} x ${num} = ${input*num}`;
+    }
+}
+function check8() {
+
+    for (let num=1;num<=4;num++) {
+        let str = "";
+        for(let num2=1; num2<=5; num2++) {
+            str += num2;
+            console.log(str);
+        }
+    }
+}
+/* function check9() {
+    let result = "";
+    for (let num=1; num<=5; num++) {
+        result += num;
+        console.log(result);
+    }
+} */
+function check9() {
+    for (let num1=1; num1<=5; num1++) {
+        let result = "";
+        for (let num2=1; num2<=num1; num2++) {
+            result += num2;
+        }
+        console.log(result);
+    }
+}
+// while 문 확인
+function check16() {
+    let val;
+    // prompt 창에 취소를 누르기 전까지 반복
+    // == 취소를 누르면 반복하지 않겠다
+    while(val !== null) {
+        // (!==) 동일 비교 연산자: 값, 자료형이 모두 다른경우
+        // (===) 값, 자료형이 모두 같은 경우
+        // (!=) 값이 다른경우
+        val = prompt("입력 후 확인");
+        // 확인 -> 입력한 값
+        // 취소 -> null
+        console.log(val);
+    }
+}
+function check17() {
+    const menu1 = 3000;
+    const menu2 = 3500;
+    const menu3 = 5000;
+
+    let count1 = 0;
+    let count2 = 0;
+    let count3 = 0;
+
+    let val; //prompt로 입력한 값을 저장할 변수 선언
+
+    while(val !== null) {
+        val = prompt("메뉴를 번호를 입력하세요");
+
+        switch (val) {
+            case "1" : count1++; break;
+            case "2" : count2++; break;
+            case "3" : count3++; break;
+            case null : alert("주문 완료"); break;
+            default : alert("없는 메뉴입니다"); break;
+        }
+    }
+    alert(`김밥 : ${count1}, 라면 : ${count2}, 돈가스 : ${count3}`);
+    let sum = (count1*menu1) + (count2*menu2) + (count3*menu3);
+    alert(`총 가격 : ${sum}원`);
+}
+// while => for
+function check18() {
+    // 1부터 10까지 출럭
+    // let num = 1;
+    // while(num <= 10) {
+    //     console.log(num);
+    //     num++;
+    // }
+    // 10 부터 1까지 1씩 감소
+    let num2 = 10;
+    while(num2 >= 1) {
+        console.log(num2);
+        num2--;
     }
 }
